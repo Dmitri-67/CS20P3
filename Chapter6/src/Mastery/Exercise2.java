3af353752a26dc016a9758f3c63fed86e12b9705
79129001 <79129001@L815WD220034.edu.cbe.ab.ca>
2025-11-14 11:23:45 -0700

    11:23AM

--- Chapter6/src/Mastery/Exercise2.java
+++ Chapter6/src/Mastery/Exercise2.java
@@ -3,38 +3,91 @@ package Mastery;
 import java.util.Scanner;
 
 public class Exercise2 {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
-        double number = input.nextDouble();
+        double number = input.nextDouble(); // number to convert
 
-        System.out.println("\nConvert:");
+        // Display menu
+        System.out.println("Convert:");
         System.out.println("1. Inches to Centimeters       5. Centimeters to Inches");
         System.out.println("2. Feet to Centimeters         6. Centimeters to Feet");
         System.out.println("3. Yards to Meters             7. Meters to Yards");
-        System.out.println("4. Miles to Kilometers         8. Kilometers to Miles\n");
+        System.out.println("4. Miles to Kilometers         8. Kilometers to Miles");
 
         System.out.print("Enter your choice: ");
         int choice = input.nextInt();
 
-        if (choice == 1)
-            System.out.println(number + " inches = " + (number * 2.54) + " centimeters.");
-        else if (choice == 2)
-            System.out.println(number + " feet = " + (number * 30) + " centimeters.");
-        else if (choice == 3)
-            System.out.println(number + " yards = " + (number * 0.91) + " meters.");
-        else if (choice == 4)
-            System.out.println(number + " miles = " + (number * 1.6) + " kilometers.");
-        else if (choice == 5)
-            System.out.println(number + " centimeters = " + (number / 2.54) + " inches.");
-        else if (choice == 6)
-            System.out.println(number + " centimeters = " + (number / 30) + " feet.");
-        else if (choice == 7)
-            System.out.println(number + " meters = " + (number / 0.91) + " yards.");
-        else if (choice == 8)
-            System.out.println(number + " kilometers = " + (number / 1.6) + " miles.");
-        else
-            System.out.println("Invalid choice.");
+        // Call a method to handle the choice
+        convert(choice, number);
     }
+
+
+    // Method that decides which conversion to do
+    public static void convert(int choice, double num) {
+        switch (choice) {
+            case 1:
+                inchesToCm(num);
+                break;
+            case 2:
+                feetToCm(num);
+                break;
+            case 3:
+                yardsToMeters(num);
+                break;
+            case 4:
+                milesToKm(num);
+                break;
+            case 5:
+                cmToInches(num);
+                break;
+            case 6:
+                cmToFeet(num);
+                break;
+            case 7:
+                metersToYards(num);
+                break;
+            case 8:
+                kmToMiles(num);
+                break;
+            default:
+                System.out.println("Invalid choice.");
+        }
+    }
+
+    // Conversion methods:
+    public static void inchesToCm(double x) {
+        System.out.println(x + " inches = " + (x * 2.54) + " centimeters.");
+    }
+
+    public static void feetToCm(double x) {
+        System.out.println(x + " feet = " + (x * 30) + " centimeters.");
+    }
+
+    public static void yardsToMeters(double x) {
+        System.out.println(x + " yards = " + (x * 0.91) + " meters.");
+    }
+
+    public static void milesToKm(double x) {
+        System.out.println(x + " miles = " + (x * 1.6) + " kilometers.");
+    }
+
+    public static void cmToInches(double x) {
+        System.out.println(x + " centimeters = " + (x / 2.54) + " inches.");
+    }
+
+    public static void cmToFeet(double x) {
+        System.out.println(x + " centimeters = " + (x / 30) + " feet.");
+    }
+
+    public static void metersToYards(double x) {
+        System.out.println(x + " meters = " + (x / 0.91) + " yards.");
+    }
+
+    public static void kmToMiles(double x) {
+        System.out.println(x + " kilometers = " + (x / 1.6) + " miles.");
+    }
+    
 }
