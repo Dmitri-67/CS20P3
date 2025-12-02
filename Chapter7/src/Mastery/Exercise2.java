2a52545975ccbe23fef01874d48cf46092e323ff
79129001 <79129001@L815WD220034.edu.cbe.ab.ca>
2025-12-02 14:28:29 -0700

    2:28PM

---
+++ Chapter7/src/Mastery/Exercise2.java
@@ -0,0 +1,25 @@
+package Mastery;
+
+public class Exercise2 {
+    private int number;
+
+    public Exercise2(int n) {
+        number = n;
+    }
+
+    public int getWhole() {
+        return number;
+    }
+
+    public int getOnes() {
+        return number % 10;
+    }
+
+    public int getTens() {
+        return (number / 10) % 10;
+    }
+
+    public int getHundreds() {
+        return (number / 100) % 10;
+    }
+}
---
+++ Chapter7/src/Mastery/TestExercise2.java
@@ -0,0 +1,36 @@
+package Mastery;
+import java.util.Scanner;
+
+public class TestExercise2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int n = input.nextInt();
+        Exercise2 num = new Exercise2(n);
+
+        char choice;
+
+        do {
+            System.out.println("Show (W)hole number.");
+            System.out.println("Show (O)nes place number.");
+            System.out.println("Show (T)ens place number.");
+            System.out.println("Show (H)undreds place number.");
+            System.out.println("(Q)uit");
+            System.out.print("Enter your choice: ");
+            choice = input.next().toLowerCase().charAt(0);
+
+            if (choice == 'w')
+                System.out.println("Whole number is: " + num.getWhole());
+            else if (choice == 'o')
+                System.out.println("The ones place digit is: " + num.getOnes());
+            else if (choice == 't')
+                System.out.println("The tens place digit is: " + num.getTens());
+            else if (choice == 'h')
+                System.out.println("The hundreds place digit is: " + num.getHundreds());
+
+        } while (choice != 'q');
+
+        System.out.println("Goodbye.");
+    }
+}
