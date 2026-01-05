1713dfe8aa108184a5762f740f84ec1c39906f11
79129001 <79129001@L815WD220034.edu.cbe.ab.ca>
2026-01-05 12:42:06 -0700

    12:41PM

--- GettingStarted/src/Tempurature.java
+++ GettingStarted/src/Tempurature.java
@@ -3,18 +3,33 @@
 import com.phidget22.*;
 
 public class Tempurature {
+    //Handle Exceptions | Exceptions will happen in your code from time to time. These are caused by unexpected things happening. Make sure you’ve added "throws Exception" to your main method.
     public static void main(String[] args) throws Exception{
 
-        //Create | Here you have created a TemperatureSensor object. TemperatureSensor is a class in your Phidgets library that gathers temperature data from your Phidget. 
-        TemperatureSensor temperatureSensor = new TemperatureSensor();
+        //Create | Here you've created a DigitalOutput object for your LED. An object represents how you interact with your device. DigitalOutput is a class from the Phidgets library that's used to provide a voltage to things like LEDs.
+        DigitalOutput redLED = new DigitalOutput();
+        DigitalOutput greenLED = new DigitalOutput();
 
-        //Open | Open establishes a connection between your object and your physical Phidget. You provide a timeout value of 1000  to give the program 1000 milliseconds (1 second) to locate your Phidget. If your Phidget can't be found, an exception will be thrown.
-        temperatureSensor.open(1000);
+        //Address | This tells your program where to find the device you want to work with. Your LED is connected to port 1 and your code reflects that. IsHubPortDevice must be set if you are not using a Smart Phidget (more on this later).
+        redLED.setHubPort(1);
+        redLED.setIsHubPortDevice(true);
+        greenLED.setHubPort(4);
+        greenLED.setIsHubPortDevice(true);
 
-        //Use your Phidgets | This code will print the temperature every 150ms
-        while (true) {
-            System.out.println("Temperature: " + temperatureSensor.getTemperature() + " °C" );
-            Thread.sleep(150);
+        //Open | Open establishes a connection between your object and your physical Phidget. You provide a timeout value of 1000 to give the program 1000 milliseconds (1 second) to locate your Phidget. If your Phidget can't be found, an exception will be thrown.
+        redLED.open(1000);
+        greenLED.open(1000);
+
+        //Use your Phidgets | Here is where you can have some fun and use your Phidgets! You can turn your LED on/off by setting the state to true/false. The sleep command keeps your LED on by letting 1000 milliseconds pass before turning the LED off.
+        while(true){
+            redLED.setState(true);
+            Thread.sleep(1000);
+            redLED.setState(false);
+            Thread.sleep(1000);
+            greenLED.setState(true);
+            Thread.sleep(1000);
+            greenLED.setState(false);
+            Thread.sleep(1000);
         }
     }
 }
